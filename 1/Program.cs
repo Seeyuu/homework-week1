0d85ab837476aa55b56246a009767bf96092eaef
Phetcharaphon456 <110883429+Seeyuu@users.noreply.github.com>
2023-02-07 22:06:09 +0700

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+
+    static double f(double x)
+    {
+        if (Math.Pow(x,2) <= 4)
+        { 
+            return 2; 
+        }
+        else 
+        { 
+            return 1 + f(x-((4*Math.Abs(x))/x));
+        }
+    }
+}
