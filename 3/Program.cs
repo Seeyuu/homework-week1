1ab4786660b9685b5027b92e56035b0bcede2878
Phetcharaphon456 <110883429+Seeyuu@users.noreply.github.com>
2023-02-07 22:19:21 +0700

    Add files via upload

---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+
+    static double f(double x)
+    {
+        if (x > -1 && x <= 0)
+        { 
+            return Math.Sqrt(1-(Math.Pow(x,2))); 
+        }
+        else if (x > 0 && x <=1)
+        { 
+            return -(Math.Sqrt(1-(Math.Pow(x,2))));
+        }
+        else if (x <= -1)
+        {
+            return f(x+2);
+        }
+        else
+        {
+            return f(x - 2);
+        }
+    }
+}
